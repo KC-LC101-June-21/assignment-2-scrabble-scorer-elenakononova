16999e4206ece76c72d5db0a968cbc9444478d1e
elenakononova <68666592+elenakononova@users.noreply.github.com>
2021-06-26 19:15:10 +0000

    +

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -76,11 +76,13 @@ let objSimpleScore = {name: "Simple Score", description: "Each letter is worth 1
 
 let objBonusVowels = {name: "Bonus Vowels",
   description: "Vowels are 3 pts, consonants are 1 pt.",
-  scoreFunction: vowelBonusScore  };
+  scoreFunction: vowelBonusScore};
 
 let objScrabble = {name: "Scrabble", description: "The traditional scoring algorithm.", scoreFunction: scrabbleScore};
 // Array for all 3 objects
- const scoringAlgorithms = [objSimpleScore, objBonusVowels, objScrabble];
+ const scoringAlgorithms = [{name: "Simple Score", description: "Each letter is worth 1 point.", scoreFunction: simpleScore}, {name: "Bonus Vowels",
+  description: "Vowels are 3 pts, consonants are 1 pt.",
+  scoreFunction: vowelBonusScore}, {name: "Scrabble", description: "The traditional scoring algorithm.", scoreFunction: scrabbleScore}];
 
 let scorer = 0 ;
  function scorerPrompt (num) {
